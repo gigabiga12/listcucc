0b65d19dd9440f48afcecc9f6ebef9cb045ea21a
GitDevla <pataki.david.ferenc@gmail.com>
2023-02-20 10:00:38 +0100

    Add project files.

---
+++ listcucc/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listcucc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == String.Empty)
+            {
+                label1.Text = "Adj meg valamit";
+                
+            
+            }
+            else
+            {
+                listBox1.Items.Add(textBox1.Text);
+                label1.Text = "Hozzáadtál valamit";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.Items.IndexOf(listBox1.SelectedItems));
+        }
+    }
+}
